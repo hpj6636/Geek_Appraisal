4f02374ca1616d71b51bd729f9b189eaa58c6923
hpj6636 <1121901675@qq.com>
2022-10-31 11:02:42 +0800

    Anwser

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+int main( ) 
+{
+	double a,step,x,y,Y,z;
+	printf("please enter a value\n");
+	scanf ("%lf",&a);
+	step=0.001;
+	x=0;
+	Y=a;
+	y=(1-step)*Y;
+	z=Y-y;
+	printf("    x                 y\n");
+	while(z>=0.000001)
+	{
+	    Y=y;
+		x=x+step;
+	    y=(1-step)*Y;
+	    z=Y-y;
+	    printf("%lf          %lf\n",x,Y);
+	}
 	return 0;
 }
-
